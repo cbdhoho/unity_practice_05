d0dbaa5f631e4810d2ff8588e0fabf1b9c01296c
cbdhoho <81568368+cbdhoho@users.noreply.github.com>
2023-04-09 00:33:42 +0900

    unity_practice_06-3

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -8,13 +8,13 @@ public class NewBehaviourScript : MonoBehaviour
     {
         if (Input.anyKeyDown)
             Debug.Log("플레이어가 아무 키를 눌렀습니다.");
-        if (Input.GetMouseButtonDown(0)) //좌클릭
-            Debug.Log("미사일 발사!");
-        if (Input.GetMouseButton(0))  //좌클릭
-            Debug.Log("미사일 모으는 중...");
-        if (Input.GetMouseButtonUp(0))
-            Debug.Log("슈퍼 미사일 발사!!");
 
+        if (Input.GetButtonDown("Jump"))
+            Debug.Log("점프!");
+        if (Input.GetButton("Jump"))
+            Debug.Log("점프 모으는 중...");
+        if (Input.GetButtonUp("Jump"))
+            Debug.Log("슈퍼 점프!!");
         
     }
 }
