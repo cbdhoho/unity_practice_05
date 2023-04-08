9f80a440b2ac0a01e4f7926622b42b8b5862b2e0
cbdhoho <81568368+cbdhoho@users.noreply.github.com>
2023-04-08 23:17:29 +0900

    unity_practice_06-1

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,44 +4,20 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    private void Awake()
-    {
-        Debug.Log("플레이어 데이터가 준비되었습니다.");
-    }
-
-    private void OnEnable()
-    {
-        Debug.Log("플레이어가 로그인했습니다.");
-    }
-    private void Start()
-    {
-        Debug.Log("사냥 장비를 챙겼습니다.");
-    }
-
-    private void FixedUpdate()
-    { 
-        Debug.Log("이동~");
-    }
-
     private void Update()
     {
-        Debug.Log("몬스터 사냥!!");
-    }
+        if (Input.anyKeyDown)
+            Debug.Log("플레이어가 아무 키를 눌렀습니다.");
 
-    private void LateUpdate()
-    {
-        Debug.Log("경험치 획득.");
-    }
+        if (Input.GetKeyDown(KeyCode.Return))
+            Debug.Log("아이템을 구입하였습니다.");
 
-    private void OnDisable()
-    {
-        Debug.Log("플레이어가 로그아웃했습니다.");
-    }
+        if (Input.GetKey(KeyCode.LeftArrow))
+            Debug.Log("왼쪽으로 이동 중");
 
-    private void OnDestroy()
-    {
-        Debug.Log("플레이어 데이터를 해체하였습니다.");
-    }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+            Debug.Log("오른쪽 이동을 멈추었습니다.");
 
-    
+        
+    }
 }
